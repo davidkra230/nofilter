3d5fc3b1ab5a9632f33936895f9a006039b8d749
David K <davidkra230@gmail.com>
2022-07-14 12:25:47 -0400

    test supporting 1.19.10

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.IO;
 
 namespace nofilter
@@ -7,12 +7,17 @@ namespace nofilter
     {
         static void Main(string[] args)
         {
-            //take ownership of the C:\Program Files\WindowsApps\Microsoft.MinecraftUWP_1.19.202.0_x64__8wekyb3d8bbwe\data folder
+            //take ownership of the C:\Program Files\WindowsApps\Microsoft.MinecraftUWP_1.19.202.0_x64__8wekyb3d8bbwe\data folder (folder is dynamic?)
             //then delete profanity_filter.wlist
-
-            Console.WriteLine("Minecraft Bedrock Edition Language Filter Remover v2.0.0");
-            Console.Title = "Minecraft Bedrock Edition Language Filter Remover v2.0.0";
-            if (!File.Exists("C:\\Program Files\\WindowsApps\\Microsoft.MinecraftUWP_1.19.202.0_x64__8wekyb3d8bbwe\\data\\profanity_filter.wlist"))
+            
+            //set dynamic vars
+            const string programName = "Minecraft Bedrock Edition Language Filter Remover v2.0.1 For Minecraft Bedrock Edition 1.19.10";
+            const string filterLocation = "C:\\Program Files\\WindowsApps\\Microsoft.MinecraftUWP_1.19.1003.0_x64__8wekyb3d8bbwe\\data\\profanity_filter.wlist"
+            //end of section
+            
+            Console.WriteLine(programName);
+            Console.Title = programName;
+            if (!File.Exists(filterLocation))
             {            
                 Console.WriteLine("No profanity_filter.wlist found\n[Any key to exit]");
                 Console.Title = "Aborted.";
@@ -35,13 +40,13 @@ namespace nofilter
 
             Console.WriteLine("Taking Ownership..");
             Console.Title = "Owning..";
-            //take ownership of the C:\Program Files\WindowsApps\Microsoft.MinecraftUWP_1.19.202.0_x64__8wekyb3d8bbwe\data\profanity_filter.wlist file]
-            File.SetAttributes("C:\\Program Files\\WindowsApps\\Microsoft.MinecraftUWP_1.19.202.0_x64__8wekyb3d8bbwe\\data\\profanity_filter.wlist", FileAttributes.Normal);
+            //take ownership of the file]
+            File.SetAttributes(filterLocation, FileAttributes.Normal);
             Console.WriteLine("Done.");
             Console.WriteLine("Deleting profanity_filter.wlist");
             Console.Title = "Deleting..";
             //delete profanity_filter.wlist
-            File.Delete("C:\\Program Files\\WindowsApps\\Microsoft.MinecraftUWP_1.19.202.0_x64__8wekyb3d8bbwe\\data\\profanity_filter.wlist");
+            File.Delete(filterLocation);
             Console.WriteLine("Done.");
             Console.WriteLine("Finished.\n[Any key to exit]");
             Console.Title = "Finished.";
