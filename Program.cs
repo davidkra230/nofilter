88402ba8d58d50a0718979c2ac7942347e6638b2
David K <davidkra230@gmail.com>
2022-10-15 01:10:59 -0400

    make comments "better", custom install dir prep and more misc changes.

--- Program.cs
+++ Program.cs
@@ -7,15 +7,19 @@ namespace nofilter
     {
         static void Main(string[] args)
         {
-            //take ownership of the C:\Program Files\WindowsApps\Microsoft.MinecraftUWP_1.19.202.0_x64__8wekyb3d8bbwe\data folder (folder is dynamic?)
-            //then delete profanity_filter.wlist
+            // take ownership of the C:\Program Files\WindowsApps\Microsoft.MinecraftUWP_1.19.202.0_x64__8wekyb3d8bbwe\data folder (folder is dynamic?)
+            // then delete profanity_filter.wlist
             
-            //set dynamic vars
+            // set dynamic vars
             const string programName = "Minecraft Bedrock Edition Language Filter Remover v2.0.1 For Minecraft Bedrock Edition (Universal)";
             //(better?) const string filterLocation = "C:\\Program Files\\WindowsApps\\Microsoft.MinecraftUWP_1.19.1003.0_x64__8wekyb3d8bbwe\\data\\profanity_filter.wlist";
-            var filterLocation = Directory.GetDirectories("C:\\Program Files\\WindowsApps\\", "Microsoft.MinecraftUWP_*", SearchOption.TopDirectoryOnly)[0] + "\\data\\profanity_filter.wlist";
-            //Console.WriteLine(filterLocation); //as a temp test
-            //end of section
+            
+            // the following line is meant to be "prep" for custom install directorys.
+            var customInstallDir = "C:\\Program Files\\WindowsApps\\";
+            
+            var filterLocation = Directory.GetDirectories(customInstallDir, "Microsoft.MinecraftUWP_*", SearchOption.TopDirectoryOnly)[0] + "\\data\\profanity_filter.wlist";
+            // Console.WriteLine(filterLocation); //as a temp test
+            // end of section
             
             Console.WriteLine(programName);
             Console.Title = programName;
@@ -26,7 +30,7 @@ namespace nofilter
                 Console.ReadKey();
                 return;
             }
-            //check if the user actually wants to delete the filter by asking them if they want to continue if they say y or enter then delete the file
+            // check if the user actually wants to delete the filter by asking them if they want to continue if they say y or enter then delete the file
             Console.Write("Are you sure you want to delete the profanity filter? [Y/n]");
             var input = Console.ReadKey();
             Console.WriteLine();
@@ -42,12 +46,12 @@ namespace nofilter
 
             Console.WriteLine("Taking Ownership..");
             Console.Title = "Owning..";
-            //take ownership of the file]
+            // take ownership of the file]
             File.SetAttributes(filterLocation, FileAttributes.Normal);
             Console.WriteLine("Done.");
             Console.WriteLine("Deleting profanity_filter.wlist");
             Console.Title = "Deleting..";
-            //delete profanity_filter.wlist
+            // delete profanity_filter.wlist
             File.Delete(filterLocation);
             Console.WriteLine("Done.");
             Console.WriteLine("Finished.\n[Any key to exit]");
